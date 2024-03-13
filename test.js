2fdb600c7cbc7e1b802e56d968b6716e4db1b766
Guendouz-Moulkhere <78477568+Guendouz-Moulkhere@users.noreply.github.com>
2024-03-13 11:47:15 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,12 +3,11 @@ const http = require('http');
 http.get('http://localhost:3000', (res) => {
   let data = '';
 
-  // A chunk of data has been received.
   res.on('data', (chunk) => {
     data += chunk;
   });
 
-  // The whole response has been received.
+  
   res.on('end', () => {
     if (data === 'Hello World!') {
       console.log('Test passed');
