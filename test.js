ef51fd8e1fe908b4244c6e5b84440143c638cbea
Guendouz-Moulkhere <78477568+Guendouz-Moulkhere@users.noreply.github.com>
2024-03-13 05:06:47 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,19 @@
-console.log('Running tests...');
-console.log('All tests passed!');
+const assert = require('assert');
+const http = require('http');
+
+describe('Test de l\'application', function() {
+    it('Doit retourner "Hello World!"', function(done) {
+        http.get('http://localhost:3000', function(response) {
+            var body = '';
+
+            response.on('data', function(d) {
+                body += d;
+            });
+
+            response.on('end', function() {
+                assert.equal(body, 'Hello World!');
+                done();
+            });
+        });
+    });
+});
