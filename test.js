8574aad2fe7e2f05c805289c835294ee36068c20
Guendouz-Moulkhere <78477568+Guendouz-Moulkhere@users.noreply.github.com>
2024-03-13 05:34:41 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,16 @@
-const mocha = require('mocha');
-const describe = mocha.describe;
-const it = mocha.it;
-const assert = require('assert');
 const http = require('http');
 
-describe('Test de l\'application', function() {
-    it('Doit retourner "Hello World!"', function(done) {
-        http.get('http://localhost:3000', function(response) {
-            var body = '';
+test('Doit retourner "Hello World!"', done => {
+    http.get('http://localhost:3000', response => {
+        var body = '';
 
-            response.on('data', function(d) {
-                body += d;
-            });
+        response.on('data', function(d) {
+            body += d;
+        });
 
-            response.on('end', function() {
-                assert.equal(body, 'Hello World!');
-                done();
-            });
+        response.on('end', function() {
+            expect(body).toBe('Hello World!');
+            done();
         });
     });
 });
-//
